fbc19deb330cb2f272c743140e79500aaaee3de0
Ivanov Svyatoslav <si.ivanov1212@gmail.com>
2022-10-14 19:08:51 +0300

    Changed main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
-int main()
-{
-    return 0;
+#include "../lab-food-library/src/food_containment.h"
+
+int main() {
+  food_containment::FridgeBuilder builder;
+  auto fridge = builder.AddDefaultItems().Build();
+  fridge->PrintFridge();
+  return 0;
 }
